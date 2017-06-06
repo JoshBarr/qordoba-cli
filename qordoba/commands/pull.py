3aa8a21ea1cfb75479c9e83dec1ca369f004a919
qordobafranzi <franzi@qordoba.com>
2017-06-05 20:55:06 -0700

    updated command pull, now uses api endpoint extract bulk

--- qordoba/commands/pull.py
+++ qordoba/commands/pull.py
@@ -4,9 +4,13 @@ import logging
 import os
 import shutil
 from argparse import ArgumentTypeError
+import requests, zipfile, StringIO
+import sys
+import tempfile
+
 
 from qordoba.commands.utils import mkdirs, ask_select, ask_question
-from qordoba.languages import get_destination_languages, init_language_storage, normalize_language
+from qordoba.languages import get_destination_languages, get_source_language, init_language_storage, normalize_language
 from qordoba.project import ProjectAPI, PageStatus
 from qordoba.settings import get_pull_pattern
 from qordoba.sources import create_target_path_by_pattern
@@ -55,7 +59,12 @@ def pull_command(curdir, config, force=False, languages=(), in_progress=False, u
     api = ProjectAPI(config)
     init_language_storage(api)
     project = api.get_project()
-    target_languages = list(get_destination_languages(project))
+    target_languages = get_destination_languages(project)
+    source_language = get_source_language(project)
+    source_name = source_language.code
+    source_content_type_code = None
+
+
     if languages:
         languages = validate_languges_input(languages, target_languages)
     else:
@@ -68,17 +77,28 @@ def pull_command(curdir, config, force=False, languages=(), in_progress=False, u
         log.debug('Pull only completed translations.')
         status_filter = [PageStatus.completed, ]
 
+
+    target_languages_page_ids = []
+    target_languages_ids = []
+    source_to_target_paths = {}
+
+
     for language in languages:
         is_started = False
-
         for page in api.page_search(language.id, status=status_filter):
+            milestone = None
             is_started = True
-            page_status = api.get_page_details(language.id, page['page_id'], )
+
+            target_languages_page_ids.append(page['page_id'])
+            target_languages_ids.append(language.id)
+
+            page_status = api.get_page_details(language.id, page['page_id'],  )
 
             log.info('Downloading translation file for source `{}` and language `{}`'.format(
                 format_file_name(page),
                 language.code,
             ))
+            
             milestone = None
             if in_progress:
                 milestone = page_status['status']['id']
@@ -88,10 +108,22 @@ def pull_command(curdir, config, force=False, languages=(), in_progress=False, u
                                                         source_name=page_status['name'],
                                                         content_type_code=page_status['content_type_code'])
 
+            stripped_target_path = ((target_path.native_path).rsplit('/',1))[0]
+            source_to_target_paths[language.code] = stripped_target_path
+            
+            # adding the source langauge to the target_path_of_source_language pattern for later renaming of folders in zip extraction 
+            target_path_of_source_language = create_target_path_by_pattern(curdir, source_language, pattern=pattern,
+                                                        source_name=source_name,
+                                                        content_type_code=source_content_type_code)
+
+            stripped_target_path_of_source_language = ((target_path_of_source_language.native_path).rsplit('/',1))[0]
+            source_to_target_paths[source_name] = stripped_target_path_of_source_language
+
             if os.path.exists(target_path.native_path) and not force:
                 log.warning('Translation file already exists. `{}`'.format(target_path.native_path))
                 answer = FileUpdateOptions.get_action(update_action) or ask_select(FileUpdateOptions.all,
                                                                                    prompt='Choice: ')
+                
                 if answer == FileUpdateOptions.skip:
                     log.info('Download translation file `{}` was skipped.'.format(target_path.native_path))
                     continue
@@ -100,18 +132,41 @@ def pull_command(curdir, config, force=False, languages=(), in_progress=False, u
                         target_path = ask_question('Set new filename: ', answer_type=target_path.replace)
                 # pass to replace file
 
-            res = api.download_file(page_status['id'], language.id, milestone=milestone)
-            res.raw.decode_content = True  # required to decompress content
-            # ensure to create all directories
-            mkdirs(os.path.dirname(target_path.native_path))
-            # copy content to dest path
-            with open(target_path.native_path, 'wb') as f:
-                shutil.copyfileobj(res.raw, f)
-
-            log.info('Downloaded translation file `{}` for source `{}` and language `{}`'
-                     .format(target_path.native_path,
-                             format_file_name(page),
-                             language.code))
-
         if not is_started:
             log.info('Nothing to download for language `{}`'.format(language.code))
+    
+    res = api.download_files(target_languages_page_ids, target_languages_ids)
+
+
+    r = requests.get(res, stream=True)
+    z = zipfile.ZipFile(StringIO.StringIO(r.content))
+
+    for source_path, target_path in source_to_target_paths.iteritems():
+        root = os.getcwd()
+        temp_folder_path = tempfile.mktemp()
+
+        #extract zip folder to root folder
+        zip_files =z.namelist()
+        dirs_to_extract = [d for d in zip_files if d.startswith(source_path) ]
+        z.extractall(root, dirs_to_extract)
+
+        # if target directory doesn't exist, we created it
+        root_src_dir = os.path.join(os.getcwd(), source_path)
+        root_dst_dir = os.path.join(os.getcwd(), target_path)
+
+        for src_dir, dirs, files in os.walk(root_src_dir):
+            dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+            if not os.path.exists(dst_dir):
+                os.makedirs(dst_dir)
+            for file_ in files:
+                src_file = os.path.join(src_dir, file_)
+                dst_file = os.path.join(dst_dir, file_)
+                if os.path.exists(dst_file):
+                    os.remove(dst_file)
+                shutil.move(src_file, dst_dir)
+            shutil.rmtree(root_src_dir)
+
+            log.info('Updated/added translation file to folder `{}` for source `{}` and language `{}` '
+               .format(target_path,
+                  source_path,
+                  source_path))
